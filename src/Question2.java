1f89704f15e1d52df78b4f9c2f1f2dfbe0849f61
palak-maheshwari <palak.maheshwari_cs18@gla.ac.in>
2019-08-25 21:13:06 +0530

    strings entered successfully

---
+++ src/Question2.java
@@ -0,0 +1,14 @@
+//package Question2;
+import java.util.Scanner;
+public class Question2 {
+        public static void main(String args[])
+        {
+            String c,b;
+            Scanner scanner= new Scanner(System.in);
+            System.out.println("Enter your sentences:");
+            c =scanner.nextLine();
+            b =scanner.nextLine();
+            System.out.println(c+"University "+b);
+        }
+
+    }
